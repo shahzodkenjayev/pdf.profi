379f4bf1e5331af17fdf571c5eb9a08458713a19
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-01-24 15:30:27 +0500

    Update compress_pdf.php

--- compress_pdf.php
+++ compress_pdf.php
@@ -4,10 +4,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
     $filename = $_FILES['file']['name'];
 
     // Fayl nomini 50 ta belgidan ortiq bo'lmasligini ta'minlash
-    $filename = substr($filename, 0, 50); // 50 ta belgidan ortiq nomni kesish
-
-    // Maxsus belgilarni va bo'shliqlarni tozalash
-    $filename = preg_replace('/[^A-Za-z0-9\-]/', '_', $filename); // faqat raqam, harf, va tirnoq belgilari qoladi
+    $filename = substr($filename, 0, 50);
+    $filename = preg_replace('/[^A-Za-z0-9\-\.]/', '_', $filename); // kengaytmalarni ham tozalash
 
     // Fayl kengaytmasini olish
     $fileExt = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
@@ -24,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
     if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile)) {
         // Kichraytirish
         $outputFile = $uploadDir . 'compressed_' . basename($filename);
-        $command = "gswin64c -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$outputFile $uploadFile";
+        $command = "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$outputFile $uploadFile";
         
         // Komandani bajarish
         exec($command);
@@ -32,17 +30,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
         // Natijani ko'rsatish
         echo "PDF fayl muvaffaqiyatli kichraytirildi. <a href='/pdf/uploads/" . basename($outputFile) . "'>Yangi faylni yuklab olish</a>";
 
-        // Faylni 1 soatdan keyin o'chirish
-        $timeToDelete = 60 * 60; // 1 soat (60 daqiqa * 60 soniya)
-
-        // Faylni o'chirish uchun kutish
-        sleep($timeToDelete);
-
         // Faylni o'chirish
-        unlink($uploadFile);
-        unlink($outputFile);
+        if (file_exists($uploadFile)) {
+            unlink($uploadFile);
+        }
+        if (file_exists($outputFile)) {
+            unlink($outputFile);
+        }
 
-        echo "<br>Fayl 1 soat o'tgach o'chirildi.";
+        echo "<br>Fayl o'chirildi.";
     } else {
         echo "Faylni yuklashda xatolik yuz berdi.";
     }
